6f48e5132337a1e8c520beae11ac7dfd44a467d3
dubuduyo <mgmxmg93@naver.com>
2023-04-28 20:38:24 +0900

    feat: Modal open

--- src/components/Sidabar.tsx
+++ src/components/Sidabar.tsx
@@ -2,26 +2,43 @@
 import { FullPost } from '@/model/conversation';
 import NewChat from './NewChat';
 import useSWR from 'swr';
+import { GridLoader } from 'react-spinners';
+import { useState } from 'react';
+import Modal from './Modal';
 
 export default function Sidabar() {
   const { data, isLoading: loading } = useSWR<FullPost[]>('/api/conversations');
-  console.log('data', data);
+  const [openModal, setOpenModal] = useState(false);
+  const createNewChat = async () => {};
 
   return (
     <div className="p-2 flex flex-col h-screen">
-      <div className="rounded-lg px-5 py-3 border-gray-700 border flex items-center justify-center space-x-2 hover:bg-gray-700/70 cursor-pointer text-gray-300 transition-all duration-200 ease-out">
-        <p>안녕 못해</p>
+      <div
+        onClick={createNewChat}
+        className="rounded-lg px-5 py-3 border-gray-700 border flex items-center justify-center space-x-2 hover:bg-gray-700/70 cursor-pointer text-gray-300 transition-all duration-200 ease-out"
+      >
+        <p>안녕</p>
         <NewChat />
       </div>
+      {loading && (
+        <div className="text-center mt-32">
+          <GridLoader color="red" />
+        </div>
+      )}
       {data && (
         <ul>
           {data.map((conversation) => (
-            <li key={conversation.id} className="mb-4">
+            <li
+              onClick={() => setOpenModal(true)}
+              key={conversation.id}
+              className="mb-4"
+            >
               {conversation.roomname}
             </li>
           ))}
         </ul>
       )}
+      {openModal && <Modal onClose={() => setOpenModal(false)}></Modal>}
     </div>
   );
 }
