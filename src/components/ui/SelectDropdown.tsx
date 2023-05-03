2321ed2050d88b1394752eb0b24d3b7bc74db490
dubuduyo <mgmxmg93@naver.com>
2023-05-03 12:55:50 +0900

    feat: select options

--- src/components/ui/SelectDropdown.tsx
+++ src/components/ui/SelectDropdown.tsx
@@ -1,48 +1,82 @@
-import SelectBox from '@toast-ui/select-box';
+import {
+  FormControl,
+  MenuItem,
+  OutlinedInput,
+  Select,
+  SelectChangeEvent,
+  Theme,
+  useTheme,
+} from '@mui/material';
+import React from 'react';
+
+const ITEM_HEIGHT = 48;
+const ITEM_PADDING_TOP = 8;
+const MenuProps = {
+  PaperProps: {
+    style: {
+      maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+      width: 250,
+    },
+  },
+};
+
+const membercount = ['2', '3', '4', '5'];
+
+function getStyles(name: string, personName: readonly string[], theme: Theme) {
+  return {
+    fontWeight:
+      personName.indexOf(name) === -1
+        ? theme.typography.fontWeightRegular
+        : theme.typography.fontWeightMedium,
+  };
+}
 
 export default function SelectDropdown() {
-    const selectBox = new SelectBox('#select-box', {
-      placeholder: 'Please select an option.',
-      data: [
-        {
-          label: 'Fruits',
-          data: [
-            { label: 'Apple', value: 'apple' },
-            { label: 'Banana', value: 'banana' },
-          ],
-        },
-        {
-          label: 'The quick brown fox jumps over the lazy dog.',
-          value: 'none',
-        },
-        {
-          label: 'Colors',
-          data: [
-            { label: 'Red', value: 'red' },
-            { label: 'Yellow', value: 'yellow' },
-            { label: 'Green', value: 'green', disabled: true },
-            { label: 'Blue', value: 'blue', disabled: true },
-            { label: 'Purple', value: 'purple' },
-          ],
-        },
-      ],
-      autofocus: true,
-      showIcon: false,
-      theme: {
-        'common.border': '1px solid black',
-        'common.color': 'black',
-        'item.highlighted.background': 'yellow',
-      },
-    });
+  const theme = useTheme();
+  const [personName, setPersonName] = React.useState<string[]>([]);
+  const handleChange = (event: SelectChangeEvent<typeof personName>) => {
+    const {
+      target: { value },
+    } = event;
+    setPersonName(
+      // On autofill we get a stringified value.
+      typeof value === 'string' ? value.split(',') : value
+    );
+  };
 
+  return (
+    <>
+      <FormControl sx={{ m: 1, width: 300, mt: 3 }}>
+        <Select
+          multiple
+          displayEmpty
+          value={personName}
+          onChange={handleChange}
+          input={<OutlinedInput />}
+          renderValue={(selected) => {
+            if (selected.length === 0) {
+              return <em>Placeholder</em>;
+            }
 
-  const category = [
-    { value: 'WARRIOR', label: '참전용사' },
-    { value: 'PATIENT', label: '희귀질환' },
-    { value: 'SINGLE', label: '미혼모' },
-    { value: 'COVID19', label: '코로나19' },
-  ];
-
-
-  return <SelectBox onChange={()=>} options={category}/>;
+            return selected.join(', ');
+          }}
+          MenuProps={MenuProps}
+          inputProps={{ 'aria-label': 'Without label' }}
+        >
+          <MenuItem disabled value="">
+            <em>Placeholder</em>
+          </MenuItem>
+          {membercount.map((membercount) => (
+            <MenuItem
+              key={membercount}
+              value={membercount}
+              style={getStyles(membercount, personName, theme)}
+            >
+              {membercount}
+            </MenuItem>
+          ))}
+        </Select>
+      </FormControl>
+    </>
+  );
 }
